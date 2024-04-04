cb7a4700c0486f2d41f4137fcab03c1782180554
EevaKR <127771026+EevaKR@users.noreply.github.com>
2024-04-04 10:46:07 +0300

    Update MainActivity.kt

--- base/src/main/java/com/example/android/whileinuselocation/MainActivity.kt
+++ base/src/main/java/com/example/android/whileinuselocation/MainActivity.kt
@@ -26,47 +26,6 @@ import com.google.android.material.snackbar.Snackbar
 private const val TAG = "MainActivity"
 private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
 
-/**
- *  This app allows a user to receive location updates without the background permission even when
- *  the app isn't in focus. This is the preferred approach for Android.
- *
- *  It does this by creating a foreground service (tied to a Notification) when the
- *  user navigates away from the app. Because of this, it only needs foreground or "while in use"
- *  location permissions. That is, there is no need to ask for location in the background (which
- *  requires additional permissions in the manifest).
- *
- *  Note: Users have the following options in Android 11+ regarding location:
- *
- *  * Allow all the time
- *  * Allow while app is in use, i.e., while app is in foreground (new in Android 10)
- *  * Allow one time use (new in Android 11)
- *  * Not allow location at all
- *
- * It is generally recommended you only request "while in use" location permissions (location only
- * needed in the foreground), e.g., fine and coarse. If your app has an approved use case for
- * using location in the background, request that permission in context and separately from
- * fine/coarse location requests. In addition, if the user denies the request or only allows
- * "while-in-use", handle it gracefully. To see an example of background location, please review
- * {@link https://github.com/android/location-samples/tree/master/LocationUpdatesBackgroundKotlin}.
- *
- * Android 10 and higher also now requires developers to specify foreground service type in the
- * manifest (in this case, "location").
- *
- * For the feature that requires location in the foreground, this sample uses a long-running bound
- * and started service for location updates. The service is aware of foreground status of this
- * activity, which is the only bound client in this sample.
- *
- * While getting location in the foreground, if the activity ceases to be in the foreground (user
- * navigates away from the app), the service promotes itself to a foreground service and continues
- * receiving location updates.
- *
- * When the activity comes back to the foreground, the foreground service stops, and the
- * notification associated with that foreground service is removed.
- *
- * While the foreground service notification is displayed, the user has the option to launch the
- * activity from the notification. The user can also remove location updates directly from the
- * notification. This dismisses the notification and stops the service.
- */
 class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
     private var foregroundOnlyLocationServiceBound = false
 
